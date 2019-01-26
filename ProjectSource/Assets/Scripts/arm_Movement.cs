5cf99538e707cd3b16e870f80c284374079c3b96
Bennett Lewis <bennettwlewis@hotmail.com>
2019-01-26 15:20:09 -0600

    Arm now swings on a timer a set amount of degerees.

--- ProjectSource/Assets/Scripts/arm_Movement.cs
+++ ProjectSource/Assets/Scripts/arm_Movement.cs
@@ -5,13 +5,20 @@ using UnityEngine;
 public class arm_Movement : MonoBehaviour
 {
     private float armMove;
-
+    private bool allowSwing;
+    private Transform startRotation;
+    private float coolTime;
+    
     private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
         armMove = 2.0f;
-        
+        allowSwing = true;
+
+        coolTime = 2.0f;
+        startRotation = transform;
+
     }
 
     // Update is called once per frame
@@ -19,11 +26,20 @@ public class arm_Movement : MonoBehaviour
     {
         
        armMove = Input.GetAxis("Fire3");
-        if (armMove > 0)
+        if (armMove > 0 && allowSwing)
         {
             Debug.Log("ACTION!");
-            
-            transform.RotateAround(transform.position, Vector3.back, 200 * Time.deltaTime);
+            allowSwing = false;
+            transform.RotateAround(transform.position, Vector3.forward, 8000);
+             
+            Invoke(nameof(resetSwingCooldown),coolTime);
         }
+    }
+
+    private void resetSwingCooldown()
+    {
+        transform.RotateAround(transform.position, Vector3.back, 8000);
+        
+        allowSwing = true;
     }
 }
