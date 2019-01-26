6a0a0cc535865cdd33140981bef35668af433113
Garrett Hansen <kirypto@gmail.com>
2019-01-26 17:21:41 -0600

    Fixing movement things

--- ProjectSource/Assets/Scripts/Movement.cs
+++ ProjectSource/Assets/Scripts/Movement.cs
@@ -1,18 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Dynamic;
+﻿using System;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    [SerializeField] private float inertiaOvercomeScalar = 1f;
+    [SerializeField] private float inertiaOvercomeThreshold = 1f;
+
     //Movement
     private float _xMove;
     private float _jMove;
-    public float speed;
-    public float jumpHeight;
+    public float speed = 75f;
+    public float jumpHeight = 20f;
 
-    public float jumpCool;
-    private float _jumpTime;
     public Rigidbody2D rb;
 
     private bool _allowJump;
@@ -20,14 +19,6 @@ public class Movement : MonoBehaviour
     // Start is called before the first frame update
     private void Awake()
     {
-        _xMove = 0.0f;
-        _jMove = 0.0f;
-
-        speed = 8.0f;
-        jumpHeight = 5.0f;
-
-        jumpCool = 0.0f;
-        _jumpTime = 5.0f;
         rb = GetComponent<Rigidbody2D>();
         _allowJump = true;
     }
@@ -36,17 +27,24 @@ public class Movement : MonoBehaviour
     private void FixedUpdate()
     {
         _xMove = Input.GetAxis("Horizontal");
-        Vector2 moveHorizontal = new Vector2(_xMove, 0.0f);
-        rb.AddForce(moveHorizontal * speed);
-        _jMove = Input.GetAxis("Jump");
+        if (_xMove > 0f || _xMove < 0f)
+        {
+            Vector2 moveHorizontal = new Vector2(_xMove, 0.0f);
+            float velocityMag = Mathf.Abs(rb.velocity.magnitude);
+            if (velocityMag < inertiaOvercomeThreshold)
+            {
+                moveHorizontal *= inertiaOvercomeScalar;
+            }
+            rb.AddForce(moveHorizontal * speed);
+        }
 
+        _jMove = Input.GetAxis("Jump");
         if (_jMove > 0 && _allowJump)
         {
             _allowJump = false;
             Invoke(nameof(ResetJumpCooldown), 1.5f);
             Vector2 moveJump = new Vector2(0.0f, _jMove);
             rb.AddForce((moveJump * jumpHeight), ForceMode2D.Impulse);
-            jumpCool = _jumpTime;
         }
     }
 
