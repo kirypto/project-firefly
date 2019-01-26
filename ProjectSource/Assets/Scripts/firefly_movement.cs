31314fb0df52e47e351df682153bd468c379644c
defau1t <robin5693@gmail.com>
2019-01-26 11:38:13 -0600

    Added firefly_movement script. Not done yet

---
+++ ProjectSource/Assets/Scripts/firefly_movement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class firefly_movement : MonoBehaviour
+{
+
+    private Vector2 startPosition;
+    private Vector2 direction;
+    Collider boundingBox;
+    // Start is called before the first frame update
+    void Start()
+    {
+        boundingBox = (Collider) GetComponentInParent(typeof(Collider));
+        startPosition = transform.position;
+        direction = Random.insideUnitSphere;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!boundingBox.bounds.Contains(transform.position)) {
+            print("Hi I'm not in the box!");
+        }
+    }
+}
