650ef2a52cc4e350168ca889d1a346f42547218c
Garrett Hansen <kirypto@gmail.com>
2019-01-26 16:24:09 -0600

    Code cleanup in Movement class

--- ProjectSource/Assets/Scripts/Movement.cs
+++ ProjectSource/Assets/Scripts/Movement.cs
@@ -5,55 +5,53 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    //MOvement
-    private float xMove;
-    private float jMove;
+    //Movement
+    private float _xMove;
+    private float _jMove;
     public float speed;
     public float jumpHeight;
 
     public float jumpCool;
-    private float jumpTime;
+    private float _jumpTime;
     public Rigidbody2D rb;
 
-    private bool allowJump;
+    private bool _allowJump;
+
     // Start is called before the first frame update
-    void Awake()
+    private void Awake()
     {
-        xMove = 0.0f;
-        jMove = 0.0f;
-        
+        _xMove = 0.0f;
+        _jMove = 0.0f;
+
         speed = 8.0f;
         jumpHeight = 5.0f;
 
         jumpCool = 0.0f;
-        jumpTime = 5.0f;
+        _jumpTime = 5.0f;
         rb = GetComponent<Rigidbody2D>();
-        allowJump = true;
+        _allowJump = true;
     }
 
     // Update is called once per frame
-    void FixedUpdate(){
-    
-        xMove = Input.GetAxis("Horizontal");
-        Vector2 Move_Horizontal = new Vector2(xMove, 0.0f);
-        rb.AddForce(Move_Horizontal * speed);
+    private void FixedUpdate()
+    {
+        _xMove = Input.GetAxis("Horizontal");
+        Vector2 moveHorizontal = new Vector2(_xMove, 0.0f);
+        rb.AddForce(moveHorizontal * speed);
+        _jMove = Input.GetAxis("Jump");
 
-            jMove = Input.GetAxis("Jump");
-        if (jMove > 0 && allowJump)
+        if (_jMove > 0 && _allowJump)
         {
-            allowJump = false;
+            _allowJump = false;
             Invoke(nameof(ResetJumpCooldown), 1.5f);
-            Vector2 Move_Jump = new Vector2(0.0f, jMove);
-            rb.AddForce((Move_Jump * jumpHeight), ForceMode2D.Impulse);
-            jumpCool = jumpTime;
+            Vector2 moveJump = new Vector2(0.0f, _jMove);
+            rb.AddForce((moveJump * jumpHeight), ForceMode2D.Impulse);
+            jumpCool = _jumpTime;
         }
-
     }
 
     private void ResetJumpCooldown()
     {
-        allowJump = true;
+        _allowJump = true;
     }
-    
-        
 }
