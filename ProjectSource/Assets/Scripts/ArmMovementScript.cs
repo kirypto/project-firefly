ae32658daa73a1b8702e69d2aa1a89e6efd2e3f4
Garrett Hansen <kirypto@gmail.com>
2019-01-26 21:51:49 -0600

    Made arm movement smooth

--- ProjectSource/Assets/Scripts/ArmMovementScript.cs
+++ ProjectSource/Assets/Scripts/ArmMovementScript.cs
@@ -4,33 +4,65 @@ using UnityEngine;
 
 public class ArmMovementScript : MonoBehaviour
 {
-    private float _armMove = 2.0f;
-    private bool _allowSwing = true;
-    private float _coolTime = 2.0f;
-    
-    private Rigidbody2D _rb;
-    // Start is called before the first frame update
+    private bool _isLerpingForward;
+    private float _timeStartedLerping;
+    private Quaternion _startRotation;
+    private Quaternion _endRotation;
+    private bool _isLerpingBackward;
 
-    // Update is called once per frame
     private void Update()
     {
-        
-       _armMove = Input.GetAxis("Fire3");
-        if (_armMove > 0 && _allowSwing)
+        if (0 < Input.GetAxis("Fire3") && !_isLerpingForward && !_isLerpingBackward)
         {
-            Debug.Log("ACTION!");
-            _allowSwing = false;
-            transform.RotateAround(transform.position, Vector3.forward, 135);
-             
-            Invoke(nameof(ResetSwingCooldown),_coolTime);
+            SwingArm();
         }
     }
 
-    private void ResetSwingCooldown()
+    void FixedUpdate()
     {
-        transform.RotateAround(transform.position, Vector3.back, 135);
-        
-        _allowSwing = true;
+        if(_isLerpingForward)
+        {
+            float timeSinceStarted = Time.time - _timeStartedLerping;
+            float percentageComplete = timeSinceStarted / 1f;
+ 
+            transform.rotation = Quaternion.Lerp(_startRotation, _endRotation, CalculateLerpVal(percentageComplete));
+ 
+            if(percentageComplete >= 1.0f)
+            {
+                _isLerpingForward = false;
+                _isLerpingBackward = true;
+                _timeStartedLerping = Time.time;
+ 
+                _startRotation = transform.rotation;
+                _endRotation = Quaternion.AngleAxis(-205f, Vector3.forward);
+            }
+        } else if (_isLerpingBackward)
+        {
+            float timeSinceStarted = Time.time - _timeStartedLerping;
+            float percentageComplete = timeSinceStarted / 1f;
+ 
+            transform.rotation = Quaternion.Lerp(_startRotation, _endRotation, percentageComplete);
+            if (percentageComplete >= 1.0f)
+            {
+                _isLerpingBackward = false;
+            }
+        }
+    }
+
+    private void SwingArm()
+    {
+        _isLerpingForward = true;
+        _timeStartedLerping = Time.time;
+ 
+        _startRotation = transform.rotation;
+        _endRotation = Quaternion.AngleAxis(-90f, Vector3.forward);
+    }
+
+    private static float CalculateLerpVal(float x)
+    {
+        // Black Magic
+        float e = 2.718281828459045235360287471352662497757247093699959574966f;
+        return (Mathf.Pow(e, ((x - 0.25f) * 15f)) / (Mathf.Pow(e, ((x - 0.25f) * 15f)) + 1f));
     }
 
     private void OnTriggerEnter2D(Collider2D other)
