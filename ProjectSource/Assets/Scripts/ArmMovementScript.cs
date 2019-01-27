d082421274c4740b2d9e9d0ee47ae201e54a99bb
Garrett Hansen <kirypto@gmail.com>
2019-01-26 19:17:00 -0600

    Code cleanup

--- ProjectSource/Assets/Scripts/ArmMovementScript.cs
+++ ProjectSource/Assets/Scripts/ArmMovementScript.cs
@@ -4,52 +4,44 @@ using UnityEngine;
 
 public class ArmMovementScript : MonoBehaviour
 {
-    private float armMove;
-    private bool allowSwing;
-    private Transform startRotation;
-    private float coolTime;
+    private float _armMove = 2.0f;
+    private bool _allowSwing = true;
+    private float _coolTime = 2.0f;
     
-    private Rigidbody2D rb;
+    private Rigidbody2D _rb;
     // Start is called before the first frame update
-    void Start()
-    {
-        armMove = 2.0f;
-        allowSwing = true;
-
-        coolTime = 2.0f;
-        startRotation = transform;
-
-    }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         
-       armMove = Input.GetAxis("Fire3");
-        if (armMove > 0 && allowSwing)
+       _armMove = Input.GetAxis("Fire3");
+        if (_armMove > 0 && _allowSwing)
         {
             Debug.Log("ACTION!");
-            allowSwing = false;
+            _allowSwing = false;
             transform.RotateAround(transform.position, Vector3.forward, 135);
              
-            Invoke(nameof(resetSwingCooldown),coolTime);
+            Invoke(nameof(ResetSwingCooldown),_coolTime);
         }
     }
 
-    private void resetSwingCooldown()
+    private void ResetSwingCooldown()
     {
         transform.RotateAround(transform.position, Vector3.back, 135);
         
-        allowSwing = true;
+        _allowSwing = true;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag.Equals("Firefly"))
+        if (!other.tag.Equals("Firefly"))
         {
-            Debug.Log("CAUGHT SOMETHING");
-            GameObject.FindGameObjectWithTag("MainCamera").GetComponent<MainCameraScript>().MarkFireflyCaught();
-            Destroy(other.gameObject);
+            return;
         }
+
+        Debug.Log("CAUGHT SOMETHING");
+        GameObject.FindGameObjectWithTag("MainCamera").GetComponent<MainCameraScript>().MarkFireflyCaught();
+        Destroy(other.gameObject);
     }
 }
